8948064186603694adb2a0ff6c6489fcb312c89b
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2017-05-02 16:40:58 -0700

    WIP

--- components/molecules/main.jsx
+++ components/molecules/main.jsx
@@ -1,6 +1,6 @@
 import React,{ Component } from 'react'
 import { Link } from 'react-router'
-import styles from '../style.css'
+import styles from '../../public/style.css'
 
 export default class Main extends Component{
   render(){
