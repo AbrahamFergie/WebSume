3c96e00b3a775cf673d37a5eb0895bad2c624f06
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2017-05-08 14:52:15 -0700

    hopefully fixed css issues regarding deployment

--- components/molecules/home/index.jsx
+++ components/molecules/home/index.jsx
@@ -1,36 +1,36 @@
 import React,{ Component } from 'react'
-import styles from './style.css'
-import './styler.scss'
+
+import "./styles.scss"
 
 export default class Home extends Component{
 
   render(){
     return(
       <div>
-        <div className={styles.home}><strong><u>Abraham Ferguson</u></strong><br></br><font size="5">Software Engineer Apprentice</font></div>
-        <div className={styles.skills}><strong><u>Skills</u></strong><br></br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+        <div className="home"><strong><u>Abraham Ferguson</u></strong><br></br><font size="5">Software Engineer Apprentice</font></div>
+        <div className="skills"><strong><u>Skills</u></strong><br></br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
           dolore magna aliqua.Fox hunting gunslinger prostate cancer et pit fighter cappuccino catcher? Lemmy ned flanders middle eastern despot?
           Testosterone trophy nigel mansell boxing champion big daft brush village people, big daft brush felis testosterone trophy village people nigel mansell bruce forsyth godlike boxing champion, fox hunting bruce forsyth felis boxing champion nigel mansell felis big daft brush godlike goose toothbrush village people mouth coiffure testosterone trophy. Professor plum helllloooo professor plum hello, we’re cockneys yosemite sam tache hungarian, helllloooo hello, we’re cockneys testosterone trophy professor plum yosemite sam hungarian hulk hogan andrew weatherall tache professor plum tache. Leonine movember waxy gurn I drink your milkshake kaiser bill.
         </div>
-        <div className={styles.skills2}><strong><u>More Skills</u></strong><br></br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+        <div className="skills2"><strong><u>More Skills</u></strong><br></br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
           dolore magna aliqua.Fox hunting gunslinger prostate cancer et pit fighter cappuccino catcher? Lemmy ned flanders middle eastern despot?
           Testosterone trophy nigel mansell boxing champion big daft brush village people, big daft brush felis testosterone trophy village people nigel mansell bruce forsyth godlike boxing champion, fox hunting bruce forsyth felis boxing champion nigel mansell felis big daft brush godlike goose toothbrush village people mouth coiffure testosterone trophy.
         </div>
-        <div className={styles.skills3}><strong><u>More Skills</u></strong><br></br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+        <div className="skills3"><strong><u>More Skills</u></strong><br></br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
           dolore magna aliqua.Fox hunting gunslinger prostate cancer et pit fighter cappuccino catcher? Lemmy ned flanders middle eastern despot?
           Testosterone trophy nigel mansell boxing champion big daft brush village people, big daft brush felis testosterone trophy village people nigel mansell bruce forsyth godlike boxing champion, fox hunting bruce forsyth felis boxing champion nigel mansell felis big daft brush godlike goose toothbrush village people mouth coiffure testosterone trophy.
         </div>
-        <div className={styles.education}><strong><u>Wer Da Learnin Come Frum</u></strong><br></br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+        <div className="education"><strong><u>Wer Da Learnin Come Frum</u></strong><br></br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
           dolore magna aliqua.Fox hunting gunslinger prostate cancer et pit fighter cappuccino catcher? Lemmy ned flanders middle eastern despot?
           Testosterone trophy nigel mansell boxing champion big daft brush village people, big daft brush felis testosterone trophy village people nigel mansell bruce forsyth godlike boxing champion, fox hunting bruce forsyth
         </div>
-        <div className={styles.related}><strong><u>Possible Relevant Wrevious Work History</u></strong><br></br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+        <div className="related"><strong><u>Possible Relevant Wrevious Work History</u></strong><br></br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
           dolore magna aliqua.Fox hunting gunslinger prostate cancer et pit fighter cappuccino catcher? Lemmy ned flanders middle eastern despot?
           Testosterone trophy nigel mansell boxing champion big daft brush village people, big daft brush felis testosterone trophy village people nigel mansell bruce forsyth godlike boxing champion, fox hunting bruce forsyth
         </div>
-        <div className={styles.menu}>Projects:</div>
-        <div className={styles.projects}><br></br><br></br>
-          <div className={styles.carousel}>
+        <div className="menu">Projects:</div>
+        <div className="projects"><br></br><br></br>
+          <div className="carousel">
             <div className="cube">
               <a
                 href="https://github.com/AbrahamFergie/myBlackJack">
@@ -74,7 +74,7 @@ export default class Home extends Component{
             </div>
           </div>
         </div>
-        <div className={styles.footer}>React-Powered</div>
+        <div className="footer">React-Powered</div>
       </div>
     )
   }
