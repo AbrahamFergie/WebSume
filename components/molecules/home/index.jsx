27f966723571d0625bc6875ef1e78b0215add2ee
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2017-05-12 14:23:37 -0700

    last minute styling

--- components/molecules/home/index.jsx
+++ components/molecules/home/index.jsx
@@ -15,48 +15,48 @@ export default class Home extends Component{
     let navBttn1 = 'navButton1-home', navBttn2 = 'navButton2-home', navBttn3 = 'navButton3-home', navBttn4 = 'navButton4-home'
 
     return(
-      <div className="bg-primary text-md-center">
+      <div className="text-md-center">
         <Header />
-        <div className="row bg-primary text-white text-center lead">
+        <div className="row text-center lead">
           <div className="link">
             <div>
-              <Link to="/skills">Bio-Page</Link>
+              <Link className="paint" to="/skills">Bio-Page</Link>
             </div>
           </div>
-          <div className="col-md-4 text-white">
-            <a target="_blank" href="https://github.com/AbrahamFergie">
+          <div className="col-md-4">
+            <a className="paint" target="_blank" href="https://github.com/AbrahamFergie">
               <br></br>My Github Profile:  github.com/AbrahamFergie<br></br><br></br></a>
           </div>
           <div className="linkedin">
-            <a target="_blank" href="https://www.linkedin.com/in/abraham-ferguson-971077117/">LinkedIn Profile</a>
+            <a className="paint" target="_blank" href="https://www.linkedin.com/in/abraham-ferguson-971077117/">LinkedIn Profile</a>
           </div>
         </div>
-        <div className="row bg-primary text-white">
-          <div className="col-md-4 bg-primary text-white">
-            <img className="img-responsive img-rounded" src="https://cloud.githubusercontent.com/assets/15825329/25975569/eb3eab02-3663-11e7-9578-18cb77d7ecff.png"></img>
+        <div className="row">
+          <div className="col-md-4 paint">
+            <img className="img-responsive img-rounded img-border-2" src="https://cloud.githubusercontent.com/assets/15825329/25975569/eb3eab02-3663-11e7-9578-18cb77d7ecff.png"></img>
             &nbsp;This is My BlackJack game that lets you place bets against the house
-            <a target="_blank"
+            <a className="paint" target="_blank"
               href="https://github.com/AbrahamFergie/myBlackJack">
 
               <br></br>&nbsp;Click Me To See The Repo</a>
           </div>
-          <div className="col-md-4 bg-primary text-white">
+          <div className="col-md-4 paint">
             <img className="img-responsive img-rounded" src="https://cloud.githubusercontent.com/assets/15825329/25976719/97058e72-366b-11e7-8599-2b8c2e16311b.png"></img>
             <img className="img-responsive img-rounded" src="https://cloud.githubusercontent.com/assets/15825329/25976694/6a2b2754-366b-11e7-8c7a-37276cf6eb85.png"></img>
             This is My Bookstore app that lets you search add and explore the books found in the attached database
-            <a target="_blank"
+            <a className="paint" target="_blank"
               href="https://github.com/AbrahamFergie/myBooky">
 
               <br></br>Click Me To See The Repo</a>
           </div>
-          <div className="col-md-4 bg-primary text-white">
+          <div className="col-md-4 paint">
             <img className="img-rounded img-responsive" src="https://ancientbeast.com/images/AncientBeast.png"></img>
             This is a pull request I did for the OSS game AncientBeast<br></br>
-            <a target="_blank" href="https://github.com/AbrahamFergie/AncientBeast/pull/1"><br></br>Click Me To See The Repo</a><br></br><br></br><br></br><br></br>
+            <a className="paint" target="_blank" href="https://github.com/AbrahamFergie/AncientBeast/pull/1"><br></br>Click Me To See The Repo</a><br></br><br></br><br></br><br></br>
 
             <img className="img-rounded img-responsive" src="https://img.pr0gramm.com/2015/10/26/25d3757fb55f0b14.jpg"></img>
             This is a pull request I did for the OSS app Prrr<br></br>
-          <a target="_blank" href="https://github.com/GuildCrafts/prrr/pull/136"><br></br>Click Me To See The Repo</a>
+          <a className="paint" target="_blank" href="https://github.com/GuildCrafts/prrr/pull/136"><br></br>Click Me To See The Repo</a>
 
           </div>
         </div>
